275bc34f244be9109adaa1489abd5776f7f672b0
Aditi-Atodaria <72127595+Aditi-Atodaria@users.noreply.github.com>
2021-01-03 12:13:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+//Create NameSpacing for Matter Modules
+const Engine = Matter.Engine
+const World = Matter.World
+const Constraint = Matter.Constraint
+const Bodies = Matter.Bodies
+
+function preload(){
+
+}
+function setup(){
+canvas = createCanvas(windowWidth / 2,windowHeight/1.5);
+engine = Engine.create();
+world = engine.world;
+Engine.run(engine);
+
+pendulum1 = new Pendulum(140,100,"pink");
+string1 = new Sling(pendulum1.body,{x:140,y:30})
+
+pendulum2 = new Pendulum(200,100,"pink");
+string2 = new Sling(pendulum2.body,{x:200,y:30})
+
+pendulum3 = new Pendulum(260,100,"pink");
+string3 = new Sling(pendulum3.body,{x:260,y:30})
+
+pendulum4 = new Pendulum(320,100,"pink");
+string4 = new Sling(pendulum4.body,{x:320,y:30})
+
+}
+function draw(){
+    background("teal");
+    
+ 
+   
+    
+    pendulum1.display();
+    pendulum2.display();
+    pendulum3.display();
+    pendulum4.display();
+
+    
+    
+
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(pendulum1.body,{x:mouseX,y:mouseY});
+}
